d36e4398533f22b3f565eaeb05b7ad025570d8c3
equant <nathanhendler@gmail.com>
2022-06-22 12:16:00 -0700

    pip

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='phytooracle_data',
+      version='0.5',
+      description='phytooracle_data',
+      author='Nathanial Hendler',
+      author_email='nathanhendler@gmail.com',
+      url='https://github.com/phytooracle/phytooracle_data',
+      packages=['phytooracle_data'],
+)
